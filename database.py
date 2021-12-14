226365945817627b5946b5a75151e5f19ed06e1a
Mario-Guilherme <carvalho16car@gmail.com>
2021-12-15 04:55:57 +0800

    feat: add query restaurante

--- database.py
+++ database.py
@@ -1,3 +1,4 @@
+import pymongo
 from utils.singleton import Singleton
 
 from config import DevelopmentConfig
@@ -29,3 +30,40 @@ class Database(metaclass=Singleton):
 
     def count_all_situacao_cadastral(self):
         return self.__get_database_estabelecimento()["recebimento"].count_documents({})
+
+    def query_restaurante(self):
+        exclude_data = {
+            "_id": False,
+            "CNPJ_BASICO": False,
+            "CNPJ_ORDEM": False,
+            "CNPJ_DV": False,
+            "IDENTIFICADOR_MATRIZ": False,
+            "NOME_FANTASIA": False,
+            "SITUACAO_CADASTRAL": False,
+            "DATA_SITUACAO_CADASTRAL": False,
+            "MOTIVO_SITUACAO_CADASTRAL": False,
+            "NOME_DA_CIDADE_NO_EXTERIOR": False,
+            "PAIS": False,
+            "CNAE_FISCAL_SECUNDARIA": False,
+            "TIPO_DE_LOGRADOURO": False,
+            "LOGRADOURO": False,
+            "NUMERO": False,
+            "COMPLEMENTO": False,
+            "BAIRRO": False,
+            "CEP": False,
+            "UF": False,
+            "MUNICIPIO": False,
+            "DDD_1": False,
+            "TELEFONE_1": False,
+            "DDD_2": False,
+            "TELEFONE_2": False,
+            "DDD_DO_FAX": False,
+            "FAX": False,
+            "CORREIO_ELETRONICO": False,
+            "SITUACAO_ESPECIAL": False,
+            "DATA_DA_SITUACAO_ESPECIAL": False,
+        }
+
+        return self.__get_database_estabelecimento()["recebimento"].find(
+            {"CNAE_FISCAL_PRINCIPAL": {"$regex": "^561"}}, projection=exclude_data
+        )
