979b8c8c41d56a5469a10eba5837e01f818b1cca
Mario-Guilherme <carvalho16car@gmail.com>
2021-12-15 10:36:12 +0800

    feat: create meathod situation_cadastral

--- utils/service.py
+++ utils/service.py
@@ -12,3 +12,8 @@ from utils.clean_data import CleanData
 class DataService:
     def __init__(self, database: Database) -> None:
         self.database = database
+
+    def porcent_situation_cadastral(self) -> float:
+        actives = self.database.query_situacao_cadastral_ativa()
+        all = self.database.query_all_situacao_cadastral()
+        return round(actives / all, 4)
