15e45538a35423242c7333c1e0ddd7261ba781f0
Mario-Guilherme <carvalho16car@gmail.com>
2021-12-15 12:50:00 +0800

    refactor:  call fuction create_file

--- main.py
+++ main.py
@@ -5,5 +5,4 @@ from utils.service import DataService
 if __name__ == "__main__":
     database = Database()
     service = DataService(database)
-    service.create_csv()
-    service.create_excel()
+    service.create_file()
