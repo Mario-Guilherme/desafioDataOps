f59af6762928dc7c837ac12d453c6bd5cf215039
Mario-Guilherme <carvalho16car@gmail.com>
2021-12-15 08:42:55 +0800

    feat: create method  clean_year

--- utils/clean_data.py
+++ utils/clean_data.py
@@ -5,3 +5,8 @@ from database import Database
 class CleanData:
     def __init__(self, data_base: Database) -> None:
         self.data_base = data_base
+
+    def clean_year(self, df_date) -> pd.DataFrame:
+        data_limite_final = df_date["ANO_INICIO_ATIVIDADE"] <= "2021"
+        df_data_clean = df_date[data_limite_final].copy()
+        return df_data_clean
